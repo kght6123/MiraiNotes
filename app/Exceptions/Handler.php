ccf1c64988f54ba42c8a461caf1650054e15eec6
hirotaka.koga <i.feeling.song+git@gmail.com>
2019-01-06 21:51:31 +0900

    add return json exception.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,31 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // If the request wants JSON (AJAX doesn't always want JSON)
+        if ($request->wantsJson()) {
+            // Define the response
+            $response = [
+                'errors' => 'Sorry, something went wrong.'
+            ];
+            // If the app is in debug mode
+            if (config('app.debug')) {
+                // Add the exception class name, message and stack trace to response
+                $response['exception'] = get_class($exception); // Reflection might be better here
+                $response['message'] = $exception->getMessage();
+                $response['trace'] = $exception->getTrace();
+            }
+            // Default response of 400
+            $status = 400;
+            // If this exception is an instance of HttpException
+            if ($this->isHttpException($exception)) {
+                // Grab the HTTP status code from the Exception
+                $status = $exception->getStatusCode();
+                $response['status'] = $status;
+            }
+            // Return a JSON response with the response array and status code
+            return response()->json($response, $status);
+        }
+        // Default to the parent class' implementation of handler
         return parent::render($request, $exception);
     }
 }
