aa5ff431ab1abdadd4e48d7d195eba460d1ba554
hirotaka.koga <i.feeling.song+git@gmail.com>
2019-01-16 15:16:25 +0900

    add view filetree.

--- resources/js/components/FileTreeSidebar.vue
+++ resources/js/components/FileTreeSidebar.vue
@@ -4,10 +4,13 @@
     <div id="new-file">
       <div class="dropdown-menu bg-dark text-light shadow p-3" style="width: 13em;z-index: 1024;">
         <div class="input-group">
-          <input type="text" class="form-control bg-dark text-light" value="new_file.md" placeholder="新しいファイル名を入力してください">
+          <input type="text" class="form-control bg-dark text-light" v-model="newFileName" placeholder="新しいファイル名を入力">
           <div class="input-group-append">
             <button type="button" class="btn btn-success" v-on:click="newfile"><i class="oi oi-check"></i></button>
           </div>
+        </div>
+        <div class="input-group">
+          <textarea class="form-control bg-dark text-light" v-model="newFileMarkdownText" placeholder="マークダウンを入力（任意）"></textarea>
         </div>
       </div>
     </div>
@@ -15,7 +18,7 @@
     <div id="new-folder">
       <div class="dropdown-menu bg-dark text-light shadow p-3" style="width: 12em;z-index: 1024;">
         <div class="input-group">
-          <input type="text" class="form-control bg-dark text-light" value="new_folder" placeholder="新しいフォルダ名を入力してください">
+          <input type="text" class="form-control bg-dark text-light" v-model="newFolderName" placeholder="新しいフォルダ名を入力">
           <div class="input-group-append">
             <button type="button" class="btn btn-success" v-on:click="newfolder"><i class="oi oi-check"></i></button>
           </div>
@@ -24,6 +27,12 @@
     </div>
     <!-- ツールバー -->
     <div class="sidebar-toolbar btn-group sticky-top text-light shadow w-100" role="group">
+      <button class="btn btn-dark text-center align-middle" v-on:click="backHome" v-bind:class="{ 'disabled': isHome() }">
+        <i class="mdi mdi-home-outline"></i>
+      </button>
+      <button class="btn btn-dark text-center align-middle" v-on:click="backFolder" v-bind:class="{ 'disabled': isHome() }">
+        <i class="mdi mdi-keyboard-backspace"></i>
+      </button>
       <button class="btn btn-dark text-center align-middle" v-on:click="refresh">
         <i class="mdi mdi-refresh"></i>
       </button>
@@ -37,9 +46,9 @@
       </button>
     </div>
     <!-- ファイルツリー -->
-    <div class="sticky-top of-y-auto mhp-100 hp-100">
-      <ul class="list-unstyled list-tree">
-        <li>
+    <div class="sticky-top of-y-auto mhp-100 hp-100 pl-3 pt-3 pb-3">
+      <ul class="list-unstyled list-tree" v-if="listfiles.length">
+        <!--li>
           <div class="btn-group dropdown w-100">
             <a href="#fileTree1" class="text-light dropdown-toggle dropdown-toggle-split open"
               role="button" data-toggle="collapse" aria-expanded="false"></a>
@@ -155,115 +164,180 @@
               </div>
             </li>
           </ul>
-        </li>
+        </li-->
         <li>
           <div class="btn-group dropdown w-100">
-            <a class="text-light dropdown-toggle dropdown-toggle-split file"></a>
-            <a href="#" class="nav-link text-light w-100">
-              <i class="oi oi-file"></i>
-              File.md
+            <!--a class="text-light dropdown-toggle dropdown-toggle-split file"></a--><!-- 上のclassのpl-3は暫定そち -->
+            <a href="#" class="nav-link text-primary w-100" v-on:click="showUserFile" v-bind:class="{ 'active' : !viewDriveFileId }">
+              <i class="oi oi-pin"></i>
+              Quick.md
             </a>
           </div>
         </li>
-        <li>
-          <div class="btn-group dropdown w-100">
-            <a class="text-light dropdown-toggle dropdown-toggle-split file"></a>
-            <a href="#" class="nav-link text-light w-100">
-              <i class="oi oi-file"></i>
-              File.md
-            </a>
-          </div>
-        </li>
-        <li>
-          <div class="btn-group dropdown w-100">
-            <a class="text-light dropdown-toggle dropdown-toggle-split file"></a>
-            <a href="#" class="nav-link text-light w-100">
-              <i class="oi oi-file"></i>
-              File.md
-            </a>
-          </div>
-        </li>
-        <li>
-          <div class="btn-group dropdown w-100">
-            <a class="text-light dropdown-toggle dropdown-toggle-split file"></a>
-            <a href="#" class="nav-link text-light w-100">
-              <i class="oi oi-file"></i>
-              File.md
-            </a>
-          </div>
-        </li>
-        <li>
-          <div class="btn-group dropdown w-100">
-            <a class="text-light dropdown-toggle dropdown-toggle-split file"></a>
-            <a href="#" class="nav-link text-light w-100">
-              <i class="oi oi-file"></i>
-              File.md
-            </a>
-          </div>
-        </li>
-        <li>
-          <div class="btn-group dropdown w-100">
-            <a class="text-light dropdown-toggle dropdown-toggle-split file"></a>
-            <a href="#" class="nav-link text-light w-100">
-              <i class="oi oi-file"></i>
-              File.md
+        <li v-for="file in listfiles" v-bind:key="file.id">
+          <div class="btn-group dropdown w-100" v-if="file.mimeType == folderMimeType || file.mimeType == markdownMimeType">
+            <!--a class="text-light dropdown-toggle dropdown-toggle-split file"></a--><!-- 上のclassのpl-3は暫定そち -->
+            <a href="#" class="nav-link text-light w-100" v-on:click="file.mimeType == folderMimeType ? showFolder($event, file.id) : showFile($event, file.id)" v-bind:class="{ 'active' : viewDriveFileId == file.id }">
+              <i class="oi" v-bind:class="[file.mimeType == folderMimeType ? 'oi-folder' : 'oi-file' ]"></i>
+              {{ file.name }}
             </a>
           </div>
         </li>
       </ul>
+      <p v-else class="p-3 text-light">No files!!!</p>
     </div>
   </div>
 </template>
 
 <script>
+import { out_console } from '../axios/axios-errors';
+
 export default {
+  data: function() {
+    return {
+      listfiles: [],
+      nextPageToken: null,
+      fieldNames: "id,name,parents,webContentLink,webViewLink,description,mimeType,kind,iconLink,size,thumbnailLink",
+      folderMimeType: "application/vnd.google-apps.folder",
+      markdownMimeType: "text/markdown",
+      directories: ["root"],
+      newFileName: "file.md",
+      newFolderName: "folder",
+      newFileMarkdownText: "",
+    };
+  },
   computed: {
     gtoken () {
       return this.$store.state.gtoken;
     },
+    markdown () {
+      return this.$store.state.user.markdown;
+    },
+    viewDriveFileId () {
+      return this.$store.state.viewDriveFileId;
+    },
+  },
+  watch: {
+    gtoken: function (newGtoken, oldGtoken) {
+      if(newGtoken) {
+        // gtokenが有る時にrootの一覧を表示する
+        this.refresh(null);
+      } else {
+        // gtokenが無い時に表示を元に戻す
+        this.listfiles = [];
+        this.initDir();
+      }
+    }
   },
   methods: {
-    newfile: function(event) {
-
+    showUserFile: function() {
+      this.$store.dispatch('setViewDriveFileId', null);
+      this.$store.dispatch('setMarkdown', this.markdown);
     },
-    newfolder: function(event) {
-
+    isHome: function() {
+      return this.directories.length < 2;
     },
-    refresh: function(event) {
-      $('#filetree .dropdown-toggle.open').click();// デフォルトで開く
-
-      // DriveにQueryを実行する 
-      axios.get('/api/drive', { 
+    initDir: function() {
+      while (!this.isHome()) { this.directories.pop(); }
+    },
+    backHome: function(event) {
+      this.initDir();
+      this.refresh(event);
+    },
+    backFolder: function(event) {
+      this.directories.pop();
+      this.refresh(event);
+    },
+    showFolder: function(event, id) {
+      //alert(id);
+      this.directories.push(id);
+      this.refresh(event);
+    },
+    showFile: function(event, id) {
+      // DriveのFileを取得する
+      axios.get('/api/drive/'+id, {
         params: {
-          //q: "'appDataFolder' in parents"/*"'root' in parents"*/,
-          spaces: "appDataFolder",
-          fields: "nextPageToken, files(id, name)",
-          pageSize: 10,
+          fields: this.fieldNames,
+          alt: "media",
           web: true }} )
         .then(function(response) {
-          console.log(response);
-          alert(JSON.stringify(response.data));
-
-          // if(response.data.auth) {
-          //   // レスポンス後に開き直す
-          //   //alert("認証OK");
-          //   this.$store.dispatch('setGtoken', response.data.gtoken);
-          //   $('#auth-modal').modal('hide');
-          // }
-
+          //console.log(response);
+          //alert(JSON.stringify(response.data));
+          this.$store.dispatch('setViewDriveFileId', id);
+          this.$store.dispatch('setMarkdown', response.data);
       }.bind(this))// thisを使う
       .catch(function(error) {
-        console.error('ERROR!! occurred in Backend. (drive/query/)', error);
-        //catch the error, check it has a response object with lodash 
-        if(_.has(error, 'response')) {
-          console.error("status : " + error.response.status);  
-          console.error("data : " + JSON.stringify(error.response.data));
-        } else {
-          console.error("Most likely a server timeout or an internet connection error");
-          console.error("error response property is undefined");
-        }
+        out_console(error, 'drive, drive/show/folder');
       }.bind(this));// thisを使う
-
+    },
+    newfile: function(event) {
+      // DriveにCreate(File)を実行する
+      axios.get('/api/drive/create', { 
+        params: {
+          name: this.newFileName,
+          mimeType: this.markdownMimeType,
+          parents: this.directories.slice(-1)[0],
+          //parents: "appDataFolder",
+          //spaces: "appDataFolder",
+          description: "",
+          fields: this.fieldNames,
+          data: this.newFileMarkdownText,
+          uploadType: "multipart",
+          useContentAsIndexableText: true,
+          web: true }} )
+        .then(function(response) {
+          //console.log(response);
+          //alert(JSON.stringify(response.data));
+          this.refresh(event);
+      }.bind(this))// thisを使う
+      .catch(function(error) {
+        out_console(error, 'drive, drive/create/file');
+      }.bind(this));// thisを使う
+    },
+    newfolder: function(event) {
+      // DriveにCreate(File)を実行する
+      axios.get('/api/drive/create', { 
+        params: {
+          name: this.newFolderName,
+          mimeType: this.folderMimeType,
+          parents: this.directories.slice(-1)[0],
+          //parents: "appDataFolder",
+          //spaces: "appDataFolder",
+          description: "",
+          fields: this.fieldNames,
+          useContentAsIndexableText: true,
+          web: true }} )
+        .then(function(response) {
+          //console.log(response);
+          //alert(JSON.stringify(response.data));
+          this.refresh(event);
+      }.bind(this))// thisを使う
+      .catch(function(error) {
+        out_console(error, 'drive, drive/create/folder');
+      }.bind(this));// thisを使う
+    },
+    refresh: function(event) {
+      // $('#filetree .dropdown-toggle.open').click();// デフォルトで開く
+      // DriveにQueryを実行する
+      axios.get('/api/drive', { 
+        params: {
+          q: "'" + this.directories.slice(-1)[0] + "' in parents",
+          //q: "'appDataFolder' in parents",
+          //spaces: "appDataFolder",
+          fields: "nextPageToken,files("+this.fieldNames+")",
+          pageSize: 100,
+          web: true }} )
+        .then(function(response) {
+          //console.log(response);
+          //alert(JSON.stringify(response.data));
+          if(response.data.listfiles) {
+            this.listfiles = response.data.listfiles;
+            this.nextPageToken = response.data.nextPageToken;
+          }
+      }.bind(this))// thisを使う
+      .catch(function(error) {
+        out_console(error, 'drive, drive/query');
+      }.bind(this));// thisを使う
     }
   },
   props: ['user'/*,'gtoken'*/],
